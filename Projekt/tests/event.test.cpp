149f618d35238fa0c0164e70a76d3e5288537541
Michael Czyż <mike@czyz.icu>
2023-05-12 19:23:24 +0200

    projekt: event tests setup

--- Projekt/tests/event.test.cpp
+++ Projekt/tests/event.test.cpp
@@ -1,2 +1,32 @@
 #include <gtest/gtest.h>
 #include "../include/event.h"
+
+#include <iostream>
+
+TEST(Event, inits)
+{
+  std::string name = "Example Event";
+  Date date;
+  Time time;
+  Color color;
+  std::string notes = "Lorem ipsum dolor sit amet";
+
+  Event example(name, date, time, color, notes);
+
+  EXPECT_EQ(example.getName(), name);
+}
+
+TEST(Event, getters)
+{
+
+}
+
+TEST(Event, setters)
+{
+
+}
+
+TEST(Event, operators)
+{
+
+}
